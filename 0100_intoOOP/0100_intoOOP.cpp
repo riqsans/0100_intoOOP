792c0de215a64d30ae53e3ca5cebce85ed01e45f
riqsans <thrq88@gmail.com>
2024-04-22 14:46:55 +0700

    memperbaiki error

--- 0100_intoOOP/0100_intoOOP.cpp
+++ 0100_intoOOP/0100_intoOOP.cpp
@@ -37,3 +37,18 @@ public :
 	}
 };
 
+int main() {
+	Mahasiswa mhs;
+	MataKuliah mk;
+
+	mhs.nama = "Budi";
+	mhs.umur = 20;
+	mhs.jurusan = "Teknik Informatika";
+	mhs.output();
+
+	mk.input();
+	mk.output();
+
+	return 0;
+}
+
